a9dcd51f6ddc076ea2202f96098c7658a6be2cf8
Brandon <brandonhabschied@gmail.com>
2023-10-16 16:52:46 -0400

    some part 1

--- Lab2/Lab02/Lab02.R
+++ Lab2/Lab02/Lab02.R
@@ -1,2 +1,12 @@
 Auto <- read_csv("C:/Users/brand/OneDrive/Desktop/School/DataDrivenDiscovery/Labs/Lab2/Auto.csv")
 View(Auto)
+Auto <- na.omit(Auto)
+# str(Auto) 
+# convert horsepower from char to double so it can be in the scatterplot
+Auto$horsepower <- as.numeric(Auto$horsepower)
+# scatterplot the numerical variables only
+Auto_num <- Auto[, -ncol(Auto)]
+pairs(Auto_num)
+round(cor(Auto_num), digits = 3)
+linReg <- lm(mpg ~ . , data = Auto_num)
+summary(linReg)
